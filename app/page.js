c3b55978a226c752bb5ef9cb5b73c6a256596059
Ibitowa Daniel <ibitowadanielfavour@gmail.com>
2023-11-17 06:54:57 -0800

    edited files

--- app/page.js
+++ app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl" />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
-                As a seasoned Front End Developer, I work on turning ideas into scalable products and innovative websites/ web applications. Dive into my portfolio to learn more about me and what I do.
+                As a seasoned Front End Developer, I work on turning ideas into scalable products and innovative websites and web applications. Dive into my portfolio to learn more about me and what I do. Also, please visit my Github to see more projects and links to them
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link
