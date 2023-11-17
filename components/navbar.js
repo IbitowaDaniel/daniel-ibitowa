498d96daf9d49367834cd9c62a620594846e4d20
Ibitowa Daniel <ibitowadanielfavour@gmail.com>
2023-11-17 07:03:57 -0800

    edited files

--- components/navbar.js
+++ components/navbar.js
@@ -122,7 +122,7 @@ const Navbar = () => {
           <motion.div
             initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
             animate={{ scale: 1, opacity: 1 }}
-            className="hidden min-w-[70vw] lg:flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+            className="hidden min-w-[70vw] sm:min-w-[90vw] lg:flex flex-col justify-between items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
             <nav className="flex items-center flex-col justify-center">
               <CustomMobileLink href="/" title="Home" className="" toggle={handleClick} />
               <CustomMobileLink href="/about" title="About" className="" toggle={handleClick} />
